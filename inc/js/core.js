815bd28e1f6498b5b99f38f99d5bf287a75b0162
Ash - SIlverless <ash@silverless.co.uk>
2019-03-29 16:55:02 +0000

    DEV - Sliding Experience Level

--- inc/js/core.js
+++ inc/js/core.js
@@ -284,4 +284,22 @@ if($('#mixitup-posts-from-past').length) {
     });
 }
 
+$.fn.isInViewport = function() {
+var elementTop = $(this).offset().top;
+var elementBottom = elementTop + $(this).outerHeight();
+var viewportTop = $(window).scrollTop();
+var viewportBottom = viewportTop + $(window).height();
+return elementBottom > viewportTop && elementTop < viewportBottom;
+};
+
+$(window).on('resize scroll', function() {
+  $('.experience-level').each(function() {
+    if ($(this).isInViewport()) {
+      $(this).addClass('active');
+    } else {
+      
+    }
+  });
+});
+
 });//Don't remove ---- end of jQuery wrapper
