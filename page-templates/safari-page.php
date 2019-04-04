2d2d00cf2799337c06c186d3da9ca4a90b654987
Ash - SIlverless <ash@silverless.co.uk>
2019-04-04 14:26:39 +0100

    FIX - minor adjustments to safari page script

--- page-templates/safari-page.php
+++ page-templates/safari-page.php
@@ -355,18 +355,14 @@ ACCOM REPEATER HERE
 </div><!--c-->
 
 <script>
-function openPage(pageName,elmnt,color) {
-  var i, tabcontent, tablinks;
+function openPage(pageName) {
+  var i, tabcontent;
   tabcontent = document.getElementsByClassName("tabcontent");
   for (i = 0; i < tabcontent.length; i++) {
     tabcontent[i].style.display = "none";
   }
-  tablinks = document.getElementsByClassName("tablink");
-  for (i = 0; i < tablinks.length; i++) {
-    tablinks[i].style.backgroundColor = "";
-  }
+
   document.getElementById(pageName).style.display = "block";
-  elmnt.style.backgroundColor = color;
 }
 
 // Get the element with id="defaultOpen" and click on it
